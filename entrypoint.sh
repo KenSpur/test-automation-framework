e3cb62a50a006bdd1be9a380dc7fb42e7dd1051a
Ken Spur <ken.spur@cegeka.com>
2022-09-22 16:24:07 +0200

    expand test image functionality

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,25 +3,44 @@
 # exit when any command fails
 set -e
 
+# set defaults
+generateDoc=false
+runTests=true
+
+# map params
 while [ "$1" != "" ]; do
     case $1 in
-        -f | --filter)  shift
-                        filter=$1
-                        ;;
-        * )             echo "Invalid param: $1"
-                        exit 1
+      -f | --filter)          shift
+                              filter=$1
+                              ;;
+      -gd | --generate-doc)   shift
+                              generateDoc=$1
+                              ;;
+      -rt | --run-tests)      shift
+                              runTests=$1
+                              ;;
+      * )                     echo "Invalid param: $1"
+                              exit 1
     esac
     shift
 done
 
-if [ -z "$filter" ]
+# run tests
+if [ "$runTests" != false ]
 then
-      echo "\nStarting tests without filter"
-      dotnet test QA.AcceptanceCriteria.Specs.dll
-else
-      echo "\nStarting tests with filter: $filter"
-      dotnet test QA.AcceptanceCriteria.Specs.dll --filter "$filter"
+      if [ -z "$filter" ]
+      then
+            echo "\nStarting tests without filter"
+            dotnet test QA.AcceptanceCriteria.Specs.dll
+      else
+            echo "\nStarting tests with filter: $filter"
+            dotnet test QA.AcceptanceCriteria.Specs.dll --filter "$filter"
+      fi
 fi
 
-echo "\nStarting LivingDoc generation"
-dotnet tool run livingdoc test-assembly QA.AcceptanceCriteria.Specs.dll -t TestExecution_*.json --output /mnt/out
+# generate doc
+if [ "$generateDoc" = true ]
+then
+      echo "\nStarting LivingDoc generation"
+      dotnet tool run livingdoc test-assembly QA.AcceptanceCriteria.Specs.dll -t /mnt/out/TestExecution_*.json --output /mnt/out
+fi
